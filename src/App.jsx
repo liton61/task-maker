d81c488d775c4d38d1097af811983ccb5e487012
liton61 <litonofficial61@gmail.com>
2025-01-07 22:04:15 +0600

    hero added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Footer from "./Footer";
 import Header from "./Header";
+import Hero from "./Hero";
 
 function App() {
   return (
     <>
       <Header />
+      <Hero />
       <Footer />
     </>
   );
